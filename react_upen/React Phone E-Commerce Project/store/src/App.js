e8ad54c2390bd4a6bb4f57ce4d41745f4ef48d0c
Upen Jaiswal <UpenJaiswal@github.com>
2019-07-05 11:32:45 +0530

    update

--- react_upen/React Phone E-Commerce Project/store/src/App.js
+++ react_upen/React Phone E-Commerce Project/store/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
 
 function App() {
   return (
     <React.Fragment>
-      <h3>Hello from App</h3>
-
+     <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
     </React.Fragment>
   );
 }
--- react_upen/React Phone E-Commerce Project/store/src/components/ProductList.js
+++ react_upen/React Phone E-Commerce Project/store/src/components/ProductList.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react';
-
+import Product from './Product';
 
 class ProductList extends Component{
   render(){
     return (
       <div>
-        <h3>Hello from ProductList</h3>
+        <Product />
       </div>
     )
   }
